150971e50e59acd9b3e38658218602cece01f82e
Documentation Builder <acgetchell@ucdavis.edu>
2015-11-10 12:02:53 -0800

    Automated documentation build for changeset f2d87789442249237e6a7b153e560e997297892e.

--- search/functions_6.js
+++ search/functions_6.js
@@ -6,5 +6,6 @@ var searchData=
   ['generate_5frandom_5ftimeslice',['generate_random_timeslice',['../utilities_8h.html#a1658d2dab13ed16607bf47a55d0513d7',1,'utilities.h']]],
   ['generate_5frandom_5funsigned',['generate_random_unsigned',['../utilities_8h.html#a43895b203fdb39fdeb7f485f1835bc51',1,'utilities.h']]],
   ['getenvvar',['getEnvVar',['../utilities_8h.html#acd3b19853598764475ffd026bd45fdb7',1,'utilities.h']]],
-  ['getvalue',['getValue',['../classdocopt_1_1_leaf_pattern.html#ac3f4114aa80355e18d6fb35c6dc70022',1,'docopt::LeafPattern::getValue()'],['../classdocopt_1_1_branch_pattern.html#a910412ec3c695a4927e9418664948057',1,'docopt::BranchPattern::getValue()']]]
+  ['getvalue',['getValue',['../classdocopt_1_1_leaf_pattern.html#ac3f4114aa80355e18d6fb35c6dc70022',1,'docopt::LeafPattern::getValue()'],['../classdocopt_1_1_branch_pattern.html#a910412ec3c695a4927e9418664948057',1,'docopt::BranchPattern::getValue()']]],
+  ['gmpzf_5fto_5fdouble',['Gmpzf_to_double',['../utilities_8h.html#aa8f005d233eb0291c6214ef4a9bc150f',1,'utilities.h']]]
 ];
--- search/typedefs_3.js
+++ search/typedefs_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['gmpz',['Gmpz',['../_metropolis_8h.html#a4648cc17c7db99f1d4627bcb33624219',1,'Metropolis.h']]],
-  ['gmpzf',['Gmpzf',['../_metropolis_8h.html#ab09035cfa5e10fa4ee88d95ef5fa7ac5',1,'Gmpzf():&#160;Metropolis.h'],['../_s3_action_8h.html#ab09035cfa5e10fa4ee88d95ef5fa7ac5',1,'Gmpzf():&#160;S3Action.h']]],
+  ['gmpzf',['Gmpzf',['../_metropolis_8h.html#ab09035cfa5e10fa4ee88d95ef5fa7ac5',1,'Gmpzf():&#160;Metropolis.h'],['../_s3_action_8h.html#ab09035cfa5e10fa4ee88d95ef5fa7ac5',1,'Gmpzf():&#160;S3Action.h'],['../utilities_8h.html#ab09035cfa5e10fa4ee88d95ef5fa7ac5',1,'Gmpzf():&#160;utilities.h']]],
   ['gt',['GT',['../periodic__3__complex_8h.html#ac1b186d943b11741250650d606e0b71c',1,'GT():&#160;periodic_3_complex.h'],['../periodic__3__triangulations_8h.html#af345e414c8d63ad635d397750d656cdb',1,'GT():&#160;periodic_3_triangulations.h']]],
   ['gt3',['Gt3',['../cdt-gv_8cpp.html#adf737e4ea4037acf9edd281973efa141',1,'cdt-gv.cpp']]]
 ];
